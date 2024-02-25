862e6e1fd7094c188b4b2de7ecf78cf1e9202c84
siefeldeen1 <sirsiefeldeen@gmail.com>
2024-02-25 18:55:11 +0200

    faq

--- src/pages/page_compounts/faq/Faq.jsx
+++ src/pages/page_compounts/faq/Faq.jsx
@@ -8,11 +8,11 @@ function Faq() {
             <h3 className='faq_h3'>Have any questions? Reach out to our team at support@givestation.com</h3>
 
             <div className='faq_body'>
-                <Question question={"What is givstation?"} />
-                <Question question={"How can verify my project?"} />
-                <Question question={"What type of project will qualify for a Grant?"} />
-                <Question question={"How can I increase my chances of getting funded?"} />
-                <Question question={"Is there a Token associated with GiveStation?"} />
+                <Question question={"What is givstation?"} answer={"answer"} />
+                <Question question={"How can verify my project?"} answer={"answer"} />
+                <Question question={"What type of project will qualify for a Grant?"} answer={"answer"} />
+                <Question question={"How can I increase my chances of getting funded?"} answer={"answer"} />
+                <Question question={"Is there a Token associated with GiveStation?"} answer={"answer"} />
             </div>
         </div>
     )
